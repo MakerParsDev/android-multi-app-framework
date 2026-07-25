105885272ee563d0974b9f62666813f5f1f59eed
MakerParsDev <287206929+MakerParsDev@users.noreply.github.com>
2026-07-25 03:20:57 +0300

    feat: implement basic ZikirGlanceWidget using GlanceAppWidget and GlanceAppWidgetReceiver

--- feature/widget/src/main/java/com/parsfilo/contentapp/feature/widget/ZikirGlanceWidget.kt
+++ feature/widget/src/main/java/com/parsfilo/contentapp/feature/widget/ZikirGlanceWidget.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import androidx.compose.runtime.Composable
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
-import androidx.glance.action.clickable
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import androidx.glance.appwidget.provideContent
